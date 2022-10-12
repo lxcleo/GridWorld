fe84360a0a3d78302bc2d246d73cf7f2f8705a4d
Li Xiaochen <lixiaochen@Lis-MacBook-Pro.attlocal.net>
2022-10-11 22:10:28 -0700

    Revised Value Iteration Equation

--- GridWorldEnv.py
+++ GridWorldEnv.py
@@ -7,6 +7,9 @@ class agent():
         self.row = x
         self.col = y
 
+
+        
+
 class env():
     # Action = [0,1,2,3,4] corresponding to [up(0), down(1), left(2), right(3) don't move(4)]
     def __init__(self,map,Pe,action,gama = 1.0):  
@@ -17,14 +20,16 @@ class env():
         self.Pa = 1 - Pe
         self.action = action
         self.gama = gama
+        self.table = []
+        self.reward = getGridWorld()
 
     def isBarrier(self,agent): # Check if given index is a barrier
         Bar = self.map[agent.row][agent.col] is None
-        Negative_Reward = agent.col == 4
-        Rs = agent.row == 2 and agent.col == 2
-        Rd = agent.row == 4 and agent.col == 2
+        # Negative_Reward = agent.col == 4
+        # Rs = agent.row == 2 and agent.col == 2
+        # Rd = agent.row == 4 and agent.col == 2
 
-        if Bar or Negative_Reward or Rs or Rd:
+        if Bar:
             return True
 
         else: return False
@@ -46,82 +51,91 @@ class env():
         return[up,down,left,right,True]
 
 
-    def getUpValue(self,agent):
-        return self.map[agent.row - 1][agent.col]
+    def getUpValue(self,agent,map):
+        return map[agent.row - 1][agent.col]
 
-    def getDownValue(self,agent):
-        return self.map[agent.row + 1][agent.col]
-
-
-    def getLeftValue(self,agent):
-        return self.map[agent.row][agent.col - 1]
-
-    def getRightValue(self,agent):
-        return self.map[agent.row][agent.col + 1]
+    def getDownValue(self,agent,map):
+        return map[agent.row + 1][agent.col]
 
 
-    def getSelfValue(self,agent):
-        return self.map[agent.row][agent.col]
+    def getLeftValue(self,agent,map):
+        return map[agent.row][agent.col - 1]
 
-    def getAllValue(self,agent,sur): # Get all value according to getSurronding return value
+    def getRightValue(self,agent,map):
+        return map[agent.row][agent.col + 1]
+
+
+    def getSelfValue(self,agent,map):
+        return map[agent.row][agent.col]
+
+    def getAllValue(self,agent,sur,map): # Get all value according to getSurronding return value
         cnt = []
         value = []
         if sur[0] is True:
             cnt.append(0)
-            value.append(self.getUpValue(agent))
+            value.append(self.getUpValue(agent,map))
             
 
         if sur[1] is True:
             cnt.append(1)
-            value.append(self.getDownValue(agent))
+            value.append(self.getDownValue(agent,map))
 
         if sur[2] is True:
             cnt.append(2)
-            value.append(self.getLeftValue(agent))
+            value.append(self.getLeftValue(agent,map))
 
         if sur[3] is True:
             cnt.append(3)
-            value.append(self.getRightValue(agent))
+            value.append(self.getRightValue(agent,map))
 
         cnt.append(4)
-        value.append(self.getSelfValue(agent))
+        value.append(self.getSelfValue(agent,map))
 
         return [cnt,value]
 
 
 
-    def calculateValue(self,agent):
+    def calculateValue(self,agent,map):
         value_sum = 0
         sur = self.getSurrounding(agent)
-        cnt, value = self.getAllValue(agent,sur)
+        cnt, value = self.getAllValue(agent,sur,map)
         index = value.index(max(value))
         max_value = value[index]
         # value_sum = self.Pa * (self.getSelfValue(agent) + self.gama * max_value) # Adding self value to value iteration
-        value_sum = self.Pa * (self.gama * max_value)
         P_error = self.Pe / 4
         false_cnt = 0
         for _ in sur:
             if _ is False:
-                false_cnt += 1
+                false_cnt += 1 # Cnt how many barriers are around the agent
 
-        value.remove(max_value)
-        P_not_move = (false_cnt + 1) * P_error
-        for i, v in enumerate(value):
-            if(i == len(value) - 1):
-                # value_sum += P_not_move * (self.getSelfValue(agent) + self.gama * value[-1]) # Probablity of not moving * value of not moving
-                value_sum += P_not_move * (self.gama * value[-1])
-            else:
-                # value_sum += P_error * (self.getSelfValue(agent) + self.gama * v)
-                value_sum += P_error * (self.gama * v)
+        if(index == len(value) - 1): # If staying is the best action
+            P_not_move = (false_cnt) * P_error # Probability for not moving
+            value_sum = (self.Pa + P_not_move) * (self.getSelfValue(agent,self.reward) + self.gama * max_value)
+            value.remove(max_value) # Remove value on the correct direction
+            for v in value:
+                value_sum += P_error * (self.getSelfValue(agent,self.reward) + self.gama * v)
+
+        else:
+            value_sum = (self.Pa) * (self.getSelfValue(agent,self.reward) + self.gama * max_value)
+            value.remove(max_value)
+            P_not_move = (false_cnt + 1) * P_error # Probability for not moving
+
+            for i, v in enumerate(value):
+                if(i == len(value) - 1):
+                    # value_sum += P_not_move * (self.getSelfValue(agent) + self.gama * value[-1]) # Probablity of not moving * value of not moving
+                    value_sum += P_not_move * (self.getSelfValue(agent,self.reward) + self.gama * value[-1])
+                else:
+                    # value_sum += P_error * (self.getSelfValue(agent) + self.gama * v)
+                    value_sum += P_error * (self.getSelfValue(agent,self.reward) + self.gama * v)
 
         return value_sum
 
 
-    def ValueIteration(self,agent,mp):      
+    def ValueIteration(self,agent,mp):   
         while(agent.row < self.h):
             while(agent.col < self.w):
                 if self.isBarrier(agent) is False:
-                    mp[agent.row][agent.col] = self.calculateValue(agent)
+                    mp[agent.row][agent.col] = round(self.calculateValue(agent,self.map),3)
                     # self.map[agent.row][agent.col] = map_re[agent.row][agent.col]
                     agent.col += 1
                 else: 
@@ -133,18 +147,18 @@ class env():
 
         agent.row = 0
         agent.col = 0
+        self.map = mp
         return mp
 
 
+    def policyIteration(self, agent, table):
+
+        return 1
 
 
 
-
-
-
-
-
-
+    
+        
 
 def getGridWorld():
     w, h = (5, 5)
@@ -171,22 +185,25 @@ def getGridWorld():
 
 
 
+def main():
 
-
-
-if __name__ == '__main__':
-    mp1 = getGridWorld()
-    mp2 = getGridWorld()
+    mp1 = getGridWorld() # k = 1
+    mp2 = getGridWorld() # k = 1
     ag = agent(0,0)
     action = [0,1,2,3,4]
     gama = 0.8
     E = env(mp1,0.5,action, gama)
-    mp2 = E.ValueIteration(ag,mp2)
+    mp2 = E.ValueIteration(ag,mp2) # k = 2
     while abs(mp1[4][1] - mp2[4][1]) > 0.01:
         mp1 = E.ValueIteration(ag,mp1)
         mp2 = E.ValueIteration(ag,mp2)
 
 
-    print(mp1)
+    return mp1
 
-    print(222)
+if __name__ == '__main__':
+    a = main()
+
+    print(1)
+
+    
